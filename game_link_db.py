fc8ea92ead47aaa91fff606a6abbc31ada9c9ce4
nonop786 <nonop786@gmail.com>
2026-01-09 11:15:42 +0100

    creation du projet et des fichiers utiles

---
+++ game_link_db.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Pajani Arnaud _ 19/12/2025
+Gestion de la BD
+"""
+connexion = sql.connect("rpg.bd")
+cursor = connexion.cursor()
+
+
+def recup_joueur(): 
+    """
+    permet d'obtenir les id, les pseudo des joueurs sous forme d'une liste de tuple
+    """
+    cursor.execute("SELECT identifiant, pseudo FROM joueurs")
+    perso = cursor.fetchall()
+    return perso # liste de tuple
