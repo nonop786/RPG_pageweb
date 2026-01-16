bb68c6554f38c9b118ece2bb1af651e478f0bc2e
JayavarmanV <122731175+JayavarmanV@users.noreply.github.com>
2026-01-16 11:51:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,17 @@
 import game_link_db as gldb
 import mouvement as mouv
 import actions as act
+import tkinter as tk
+from tkinter import ttk
 
 """ TO DO
 creer les fonctions de déplacement
 """
+
+
+root = tk.Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
