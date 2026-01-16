11b99e52e2ff2ca26ca2ff6d84fd87160a535589
Arnaud P <nonop786@gmail.com>
2026-01-16 11:29:05 +0100

    Update main.py
    ajout des premières dependances

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-# Fichier main (exécution du code)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Fichier main (exécution du code _ "serveur")
 
-# TO DO
-# Ajouter dependances
+import game_link_db as gldb
+import mouvement as mouv
+import actions as act
+
+""" TO DO
+creer les fonctions de déplacement
+"""
